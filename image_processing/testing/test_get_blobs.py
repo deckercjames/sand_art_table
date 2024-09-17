6bda842eda8e3b1b481fe7327cd6bffa356b02ab
James Decker <deckej5@rpi.edu>
2024-09-16 21:44:21 -0400

    added test casees

--- image_processing/testing/test_get_blobs.py
+++ image_processing/testing/test_get_blobs.py
@@ -141,6 +141,38 @@ def test_get_blobs_one_with_void():
     assert test_pixel_grid == test_pixel_grid_unchanged
 
 
+def test_get_blobs_one_with_two_voids():
+    test_pixel_grid = [
+        [1, 1, 1, 1, 1],
+        [1, 0, 1, 0, 1],
+        [1, 1, 1, 1, 1],
+    ]
+    test_pixel_grid_unchanged = deepcopy(test_pixel_grid)
+    recv_blobs = get_blobs(test_pixel_grid)
+    expected_blobs = [
+        BlobTuple(
+            # Blob mask
+            [
+                [True,  True,  True,  True,  True],
+                [True,  False, True,  False, True],
+                [True,  True,  True,  True,  True],
+            ],
+            # Outer contour
+            [(0,0), (0,1), (0,2), (0,3), (0,4), (0,5), (1,5), (2,5), (3,5), (3,4), (3,3), (3,2), (3,1), (3,0), (2,0), (1,0)],
+            # Void contours
+            [
+                [(1,1), (1,2), (2,2), (2,1)],
+                [(1,3), (1,4), (2,4), (2,3)],
+            ],
+            # Sub-blobs
+            [],
+        ),
+    ]
+    assert len(recv_blobs) == len(expected_blobs)
+    assert recv_blobs == expected_blobs
+    assert test_pixel_grid == test_pixel_grid_unchanged
+
+
 def test_get_blobs_nested():
     test_pixel_grid = [
         [1, 1, 1],
@@ -174,6 +206,59 @@ def test_get_blobs_nested():
                     ],
                     # Outer contour
                     [(1,1), (1,2), (2,2), (2,1)],
+                    # Void contours
+                    [],
+                    # Sub-blobs
+                    [],
+                ),
+            ],
+        ),
+    ]
+    assert len(recv_blobs) == len(expected_blobs)
+    assert recv_blobs == expected_blobs
+    assert test_pixel_grid == test_pixel_grid_unchanged
+
+
+
+def test_get_blobs_nested_with_void_buffer():
+    test_pixel_grid = [
+        [1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 2, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1],
+    ]
+    test_pixel_grid_unchanged = deepcopy(test_pixel_grid)
+    recv_blobs = get_blobs(test_pixel_grid)
+    expected_blobs = [
+        BlobTuple(
+            # Blob mask
+            [
+                [True,  True,  True,  True,  True],
+                [True,  False, False, False, True],
+                [True,  False, False, False, True],
+                [True,  False, False, False, True],
+                [True,  True,  True,  True,  True],
+            ],
+            # Outer contour
+            [(0,0), (0,1), (0,2), (0,3), (0,4), (0,5), (1,5), (2,5), (3,5), (4,5), (5,5), (5,4), (5,3), (5,2), (5,1), (5,0), (4,0), (3,0), (2,0), (1,0)],
+            # Void contours
+            [
+                [(1,1), (1,2), (1,3), (1,4), (2,4), (3,4), (4,4), (4,3), (4,2), (4,1), (3,1), (2,1)],
+            ],
+            # Sub-blobs
+            [
+                BlobTuple(
+                    # Blob mask
+                    [
+                        [False, False, False, False, False],
+                        [False, False, False, False, False],
+                        [False, False, True,  False, False],
+                        [False, False, False, False, False],
+                        [False, False, False, False, False],
+                    ],
+                    # Outer contour
+                    [(2,2), (2,3), (3,3), (3,2)],
                     # Void contours
                     [],
                     # Sub-blobs
